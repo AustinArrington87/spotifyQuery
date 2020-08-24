5400284a64608e250a0d17a5fe3cecbe3fbbd67f
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2020-08-24 12:00:33 -0700

    added tempo

--- topTracks.py
+++ topTracks.py
@@ -49,46 +49,93 @@ for uri in URIs:
     
 danceList = []
 danceStat = []
+bpmStat = []
 # for each track_id 
 for track_id in trackIDs:
     danceDic = {}
     r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
     r = r.json()
+    # audio features
     dance = r['danceability']
     danceStat.append(dance)
+    # tempo
+    tempo = r['tempo']
+    bpmStat.append(tempo)
+    # key
+    key = r['key']
+    if key == 0:
+        key = "C"
+    elif key == 1:
+        key = "C#/Db"
+    elif key == 2:
+        key = "D"
+    elif key == 3:
+        key = "D#/Eb"
+    elif key == 4:
+        key = "E"
+    elif key == 5:
+        key = "F"
+    elif key == 6:
+        key = "F#/Gb"
+    elif key == 7:
+        key = "G"
+    elif key == 8:
+        key = "G#/Ab"
+    elif key == 9:
+        key = "A"
+    elif key == 10:
+        key = "A#/Bb"
+    elif key == 11:
+        key = "B"
+    else:
+        key = "None"
     # song name
     s = requests.get(BASE_URL + 'tracks/' + track_id, headers=headers)
     s = s.json()['name']
     print(s+"____"+str(dance))
+    print("tempo: "+str(tempo)+" | " + "key: " +str(key))
     danceDic["song"] = s
     danceDic["danceability"] = dance
+    danceDic['tempo'] = tempo
+    danceDic['key'] = key
     danceList.append(danceDic)
 
 danceMin = min(danceStat)
 danceMax = max(danceStat)
 danceMean = round(statistics.mean(danceStat),2)
-print("Min Danceability:", danceMin)
-print("Max Danceability:", danceMax)
-print("Mean Danceability:", danceMean)
-# now sort dic and find top 10 songs in terms of danceability
+bpmMin = min(bpmStat)
+bpmMax = max(bpmStat)
+bpmMean = round(statistics.mean(bpmStat),2)
+print("-----------------------------------")
+print("Min Danceability:", danceMin, "| Min BPM:", bpmMin)
+print("Max Danceability:", danceMax, "| Max BPM:", bpmMax)
+print("Mean Danceability:", danceMean, "| Mean BPM:", bpmMean)
+# now sort dic and find top 5 songs in terms of danceability
 newlist = sorted(danceList, key=lambda k: k['danceability']) 
 topTenList = newlist[-5:]
-print("Top 5 2020 Tracks for Danceability")
 print("-----------------------------------")
+print("Top 5 2020 Tracks for Danceability & Tempo")
 topTenSong = []
 topTenScore = []
+topTenTempo = []
 for i in topTenList:
-    print(i['song'] + ':' + str(i['danceability']))
+    print(i['song'], '| DanceScore:', i['danceability'], '| BPM:', i['tempo'])
     topTenSong.append(i['song'])
     topTenScore.append(i['danceability'])
+    topTenTempo.append(i['tempo'])
 
 plt.rcParams["figure.figsize"] = (8, 4)
 plt.rcParams["xtick.labelsize"] = 7
 plt.bar(topTenSong, topTenScore)
-# reshape - 8 wide by 4 high
+# reshape - 8 wide by 4 high - so text doesn't overlap
 plt.xlabel('Song')
 plt.ylabel('Danceability')
 plt.title('2020 Top 5 Songs for Dancing')
+plt.show()
+plt.bar(topTenSong, topTenTempo)
+plt.xlabel('Song')
+plt.ylabel('Tempo')
+plt.title('2020 Top 5 Songs BPM')
 plt.show()
 
 
